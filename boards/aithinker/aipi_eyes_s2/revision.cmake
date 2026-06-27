eb2d7b6ccf03535b303dec9e614d7dd145fe2822
Josuah Demangeon <me@josuah.net>
2026-06-27 19:11:16 +0200

    boards: bflb: Introduce aipi_eyes_s2
    Introduce an alternative to ai_m61_32s_kit with more connectors.
    
    Signed-off-by: Josuah Demangeon <me@josuah.net>

---
+++ boards/aithinker/aipi_eyes_s2/revision.cmake
@@ -0,0 +1,8 @@
+set(BOARD_REVISIONS "ALL")
+if(NOT DEFINED BOARD_REVISION)
+  set(BOARD_REVISION "ALL")
+else()
+  if(NOT BOARD_REVISION IN_LIST BOARD_REVISIONS)
+    message(FATAL_ERROR "${BOARD_REVISION} is not a valid revision for aipi_eyes_s2 Accepted revisions: ${BOARD_REVISIONS}")
+  endif()
+endif()
